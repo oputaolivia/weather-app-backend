cd11e172c48a0fe461904fc388fff4fba178932f
Donald Peters <donny9585@gmail.com>
2025-07-24 23:12:12 +0100

    feat: add endpoint to fetch weather forecast by coordinates

--- src/routers/weather.router.js
+++ src/routers/weather.router.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { getWeatherData } from '../controllers/weather.controller.js'
+import {getForecastByCoordinate, getForecastByState} from '../controllers/weather.controller.js'
 
 const weatherRouter = Router();
 
-weatherRouter.post('/', getWeatherData);
+weatherRouter.post('/by-state', getForecastByState);
+weatherRouter.post('/by-coordinates', getForecastByCoordinate);
 
 export default weatherRouter;
